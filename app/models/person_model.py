b519a24b631191861a9973141ee44682c28d91b4
Caal Quintanilla <cdcaalqu@sat.gob.gt>
2024-10-23 16:04:09 -0600

    se ajusta nombre al modelo de las variables contenidads.

--- app/models/person_model.py
+++ app/models/person_model.py
@@ -2,14 +2,14 @@ from ..database import db
 
 class Person(db.Model):
 
-    __tablename__ = 'person'  # Nombre de la tabla
-    __table_args__ = {'schema': 'ap_general'}  # Especifica el esquema
+    __tablename__ = 'person'
+    __table_args__ = {'schema': 'ap_general'}
     
     id_person = db.Column(db.String(50), primary_key=True)    
     Id_company = db.Column(db.String(15), db.ForeignKey('ap_general.company.Id_company'), nullable=False)
     name = db.Column(db.String(150), nullable=False)
     lastname = db.Column(db.String(150), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
 
     #columnas de auditoria.
     status_per = db.Column(db.String(1), nullable=False)
